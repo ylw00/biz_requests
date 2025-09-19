b2697892ecc1989d5fecea69fb0ce801a36fda82
ylw200001 <2783693683@qq.com>
2025-09-19 15:20:20 +0800

    修复默认headers 可能报错 功能

--- request/headers.py
+++ request/headers.py
@@ -18,8 +18,6 @@ from tools.cookies import CookieTools
 class Headers(OrderedDict):
     __default_head = {
         "accept": "*/*",
-        "accept-language": "zh-CN,zh;q=0.9",
-        "content-type": "application/json;charset=UTF-8",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
     }
 
